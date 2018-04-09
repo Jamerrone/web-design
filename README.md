27a65bf91f103d55a5123fa64e881100c89fe632
James Peter Perrone Jefferies <james.p.perrone@gmail.com>
2018-04-09 20:49:18 +0200

    Image slider version 1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # web-design
+
+## Image Source
+
+* https://dribbble.com/shots/4445673-Landscape-of-Northern-Lights
+* https://dribbble.com/shots/4448926-Team-two
+* https://dribbble.com/shots/4447459-Crisscross-Plane
+* https://dribbble.com/shots/4448757-Bring-cool-in-the-summer
+* https://dribbble.com/shots/4447193-Slippy
+* https://dribbble.com/shots/4449252-Analytics-Data
+* https://dribbble.com/shots/4448800-Look-up
+* https://dribbble.com/shots/4448351-Hello-Dribbble
+* https://dribbble.com/shots/4448623-Warrior-Page
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,36 @@
 import '../styles/index.css';
+
+const DIALOG = document.getElementById('dialog');
+const DIALOG_IMAGE = dialog.querySelector('img');
+
+window.onload = () => {
+  const LAST_ITEM = document.querySelector('.item:last-of-type');
+  const FIRST_ITEM = document.querySelector('.item:first-of-type');
+  const LOADER = document.getElementById('loader');
+  LAST_ITEM.scrollIntoView();
+
+  window.setTimeout(() => {
+    LOADER.hidden = true;
+    FIRST_ITEM.scrollIntoView();
+  }, 1000);
+};
+
+document.querySelectorAll('.item').forEach((item) => {
+  item.addEventListener('click', () => {
+    const IMAGE_SRC = item.querySelector('img').src;
+    DIALOG_IMAGE.src = IMAGE_SRC;
+  });
+});
+
+DIALOG_IMAGE.addEventListener('click', () => {
+  DIALOG_IMAGE.classList.toggle('fullWidth');
+});
+
+document.addEventListener('click', (e) => {
+  const TARGET = DIALOG_IMAGE.contains(e.target);
+  if (DIALOG.classList.contains('open') && !TARGET) {
+    return DIALOG.classList.remove('open');
+  } else {
+    return DIALOG.classList.add('open');
+  }
+});
